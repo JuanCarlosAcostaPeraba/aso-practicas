9e175ae172c0ac82d45b579cb3ae1a4f9387f2ac
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2023-11-10 14:17:35 +0000

    Add script for creating backups with different levels

---
+++ pract-4/copias_seguridad.sh
@@ -0,0 +1,40 @@
+#!/bain/bash
+
+error()
+{
+	echo $1 >&2
+	exit $2
+}
+
+(( $# != 1 )) && error "Error: Indique el nivel de copia" 1
+
+dir="$HOME/prueba"
+backup="$HOME/backups"
+sufijo="nivel$1_$(date +%Y%m%d_%H%M)"
+testigo="$backup/testigo.nivel0"
+
+case $1 in
+0)
+	touch $testigo
+	find $dir -print0 | \
+	tar zcvf "$backup/backup$sufijo.tgz" --null --no-recursion -T- \
+	> "$backup/catalogo$sufijo.txt" \
+	2> "$backup/errores$sufijo.txt"
+	;;
+1)
+	if [ -e $testigo ]
+	then
+		find $dir --newer $testigo -print0 | \
+		tar zcvf "$backup/backup$sufijo.tgz" --null --no-recursion -T- \
+		> "$backup/catalogo$sufijo.txt" \
+		2> "$backup/errores$sufijo.txt"
+	else
+		$HOME/bin/backup.sh 0
+	fi
+	;;
+*)
+	error "Error: Nivel de copia incorrecto" 2
+	;;
+esac
+
+exit 0
